10a00f451574a1dad83b4adaff5094bf16a75944
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-05 17:24:37 -0400

    adding vars to liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,24 @@
 var key1 = require("./keys.js");
+var request = require("request");
+var first = process.argv[2];
+var second = process.argv[3];
 
 var twitter = process.argv[2];
+
+request(url, function(err, response, body) {
+	body = JSON.parse(body);
+	console.log(body.Released);
+})
+
+var twitterURL ="https://api.twitter.com/oauth/request_token";
+
+//call names from terminal
+function startApp(first, second){
+	switch(first){
+		case 'my-tweets':
+		//calls twitter
+			myTweets();
+	};
+}
+
+startApp(first,second);
