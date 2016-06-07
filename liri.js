b1a642530c3d24c1e0dd1af2975ab5f92abab3cb
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-06 22:30:29 -0400

    added comments

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//list of global vars
 var key1 = require("./keys.js");
 var keyT = key1.twitterKeys;
 var request = require("request");
@@ -5,15 +6,6 @@ var first = process.argv[2];
 var second = process.argv[3];
 var fs = require('fs');
 
-
-// var twitter = process.argv[2];
-
-// request(url, function(err, response, body) {
-// 	body = JSON.parse(body);
-// 	console.log(body.Released);
-// });
-
-// var twitterURL ="https://api.twitter.com/oauth/request_token";
 
 //call names from terminal
 function startApp(first, second){
@@ -42,8 +34,9 @@ function myTweets(){
 
 	var Twitter = require('twitter');
 	var client = new Twitter(keyT);
+	//This will grab the twitter username and the last 20 tweets
 	client.get('statuses/user_timeline', {screen_name: 'gusmano84', count: 20}, function(error, tweets, response) {
-	
+		//loop the tweets to be logged
 		for (var prop in tweets) {
   			console.log(tweets[prop].text);
    			console.log(tweets[prop].created_at);
@@ -77,13 +70,13 @@ function movieThis(){
     	}
 	}
 
-	// Then run a request to the OMDB API with the movie specified 
+	// Run Request to OMDB API with movie name
 	var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&y=&plot=short&tomatoes=true&r=json';
 
 
 	request(queryUrl, function (error, response, body) {
 
-    	// If the request is successful (i.e. if the response status code is 200)
+    	
     	if (!error && response.statusCode == 200) {
 
     		// if (movieName= "") {
@@ -92,8 +85,7 @@ function movieThis(){
 
         
 
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
+      	//Parse through IMBD data and console long the below objects
         console.log("Title: " + JSON.parse(body)["Title"]);
         console.log("Released: " + JSON.parse(body)["Year"]);
         console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
@@ -102,6 +94,7 @@ function movieThis(){
         console.log("Actors: " + JSON.parse(body)["Actors"]);
         console.log("Rotton Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
 
+        //store the returns in log.txt
        	fs.appendFile("./log.txt",' \nTitle: '+ JSON.parse(body)["Title"] + ' \nYear: ' + JSON.parse(body)["Year"]  + ' \nIMBD Rating: ' + JSON.parse(body)["imdbRating"] + ' \nCountry: ' + JSON.parse(body)["Country"] + ' \nPlot: '+ JSON.parse(body)["Plot"] + ' \nActors: ' + JSON.parse(body)["Actors"] + ' \nRotten Tomatoes URL: '+ JSON.parse(body)["tomatoURL"], function(err) {
 	    		if(err) {
 	       	 			return console.log(err);
